70c61a4d738fc2278a4eaa353c35764d556f42fc
sofakob <hhitech628@gmail.com>
2025-05-24 21:50:02 +0300

    coments

--- laba4.py
+++ laba4.py
@@ -31,6 +31,7 @@ def generator_Giffi(x:list, y:list, s:list):
 
 def for_R(arr_x:list, arr_z:list, N:int):
     r=0
+    """Рахуємо статистику"""
     for i in range(N-1):
         r+=arr_x[i]^arr_z[i]
     return r
@@ -39,6 +40,7 @@ def stvoruemo_kupu_L():
     l=[]
     l_1=[0]*30
     k=30
+    """Вже не потрібна функція бо пішли інакшим чином"""
     l.append(l_1[:])
     for i in range(1, 31):
         l_1[k-i]=1
@@ -56,20 +58,22 @@ z=[int(x) for x in bits]
 
 alpha=0.01
 l_1_bits=[0]*30
+#Створили L1 де усі 1, в наступній строчці в останій біт засунули одиничку
 l_1_bits[-1]=1
 l_1_bits=l_1(l_1_bits, k)
+#нагенерували послідовність L1
 statistik_r=[0]*k
 pihod_1=[]
 c=80
 print(len(l_1_bits))
 for i in range(k):
-    statistik_r[i]=for_R(l_1_bits, z, k)
-    if statistik_r[i]<c:
+    statistik_r[i]=for_R(l_1_bits, z, k) # рахуємо статистику
+    if statistik_r[i]<c: #вибираємо потрібні L1
         pihod_1.append(l_1_bits)
-    l_1_bits.pop(0)
+    l_1_bits.pop(0) #Видаляємо перший елемент, і догенеровуємо його
     l_1_bits=l_1(l_1_bits, k)
 
 
-print(pihod_1)
+print(pihod_1) #перевіряємо те що нарахували
 print(statistik_r)
     
